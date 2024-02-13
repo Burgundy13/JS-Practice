d25ada4f034582142a512449720fca739b023895
Vlado Ivkovic <ivkovicv90@gmail.com>
2024-02-13 13:10:48 +0100

    Name number done

---
+++ Name number/main.js
@@ -0,0 +1,90 @@
+const input = document.querySelector('input');
+const startBtn = document.querySelector('#show');
+const showRes = document.querySelector('.show-result');
+
+let nameInputVal;
+let nameScore;
+let word;
+let wordVal;
+
+let words = {
+	A: 1,
+	J: 1,
+	S: 1,
+	Š: 1,
+	B: 2,
+	K: 2,
+	T: 2,
+	C: 3,
+	Č: 3,
+	Ć: 3,
+	L: 3,
+	LJ: 3,
+	U: 3,
+	D: 4,
+	Đ: 4,
+	DŽ: 4,
+	M: 4,
+	V: 4,
+	E: 5,
+	N: 5,
+	NJ: 5,
+	2: 5,
+	F: 6,
+	O: 6,
+	X: 6,
+	G: 7,
+	P: 7,
+	Y: 7,
+	H: 8,
+	Q: 8,
+	Z: 8,
+	Ž: 8,
+	I: 9,
+	R: 9,
+};
+
+startBtn.addEventListener('click', show);
+window.addEventListener('keyup', check);
+
+function show() {
+	nameScore = 0;
+	nameInputVal = input.value.toLocaleUpperCase();
+	checkCharacter();
+
+	if (nameInputVal.length >= 3) {
+		for (let i = 0; i < nameInputVal.length; i++) {
+			word = nameInputVal[i].split('');
+			wordVal = words[word];
+			nameScore += wordVal;
+
+			if (isNaN(parseInt(nameScore))) {
+				showRes.innerHTML =
+					'<span class="red">Please check your letters</span>';
+				showRes.style.display = 'block';
+			} else {
+				showRes.innerHTML = `Total score for <span class="red">${nameInputVal}</span> is <span class="red">${nameScore}</span>`;
+			}
+		}
+	} else {
+		showRes.innerHTML =
+			'<span class="red">Please enter a name which contains more than 3 characters</span>';
+		showRes.style.display = 'block';
+	}
+}
+
+function checkCharacter() {
+	if (nameInputVal.indexOf('DŽ') !== -1) {
+		nameScore = -8;
+	} else if (nameInputVal.indexOf('LJ') !== -1) {
+		nameScore = -1;
+	} else if (nameInputVal.indexOf('NJ') !== -1) {
+		nameScore = -1;
+	}
+}
+
+function check(e) {
+	if (e.keyCode === 13) {
+		show();
+	}
+}
